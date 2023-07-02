6a86999d7a8fb13a98b3dd0f761f3f820612d15f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-07-01 23:11:01 -0700

    Add initial nav

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -1,1 +1,16 @@
-<nav>Nick Lavecchia</nav>
+<nav class="nav">
+	<div class="logo">Nick Lavecchia</div>
+</nav>
+
+<style>
+	.nav {
+		padding: var(--space-standard);
+		position: sticky;
+		top: 0;
+		background: white;
+	}
+
+	.logo {
+		font-weight: bold;
+	}
+</style>
