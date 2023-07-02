2df332106171011eb9c08cbfb37d08790f96169d
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-07-01 23:20:42 -0700

    Add items in nav

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -1,5 +1,10 @@
 <nav class="nav">
 	<div class="logo">Nick Lavecchia</div>
+	<ul class="links">
+		<li>Portfolio</li>
+		<li>About</li>
+		<li>Fine Art</li>
+	</ul>
 </nav>
 
 <style>
@@ -8,6 +13,14 @@
 		position: sticky;
 		top: 0;
 		background: white;
+
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.links {
+		display: flex;
+		gap: var(--space-standard);
 	}
 
 	.logo {
