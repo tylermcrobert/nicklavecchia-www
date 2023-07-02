efa9a332efb60411c63f9c6ed5d39b4f77a29cad
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-07-01 23:06:13 -0700

    Add image grid

---
+++ web/src/components/ImageGrid.svelte
@@ -0,0 +1,20 @@
+<div class="imageGrid">
+	<slot />
+</div>
+
+<style lang="scss">
+	.imageGrid {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--space-standard);
+		padding: var(--space-standard);
+
+		:global(img) {
+			display: inline-block;
+			vertical-align: top;
+
+			height: 100px;
+			width: auto;
+		}
+	}
+</style>
