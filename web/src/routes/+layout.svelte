ca711468b5834a8c8c18a41af7450eaa9a4f742f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-07-01 23:50:16 -0700

    Add smooth scroll

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { Nav } from '../components';
 	import '../styles/globals.scss';
+	import Lenis from '@studio-freight/lenis';
+
+	onMount(() => {
+		const lenis = new Lenis();
+
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	});
 </script>
 
 <Nav activeSlug="root" />
