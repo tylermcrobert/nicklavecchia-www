1a5684e9f853de65af7d251d7ac696f0a7fd69cc
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-07-12 11:53:38 -0700

    refactor: add image collection component

---
+++ web/src/components/ImageCollection.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { ResponsiveImage } from '$components';
+	import type { CollectionQuery } from '$lib/sanity/queries';
+
+	export let data: CollectionQuery;
+</script>
+
+<div class="images">
+	{#each data.images as image}
+		<ResponsiveImage {image} sizes="50vh" alt="Nick LaVecchia" />
+	{/each}
+</div>
+
+<style lang="scss">
+	.images {
+		:global(img) {
+			/* height: 80vh;
+			width: auto; */
+		}
+	}
+</style>
--- web/src/routes/collection/[slug]/+page.svelte
+++ web/src/routes/collection/[slug]/+page.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
+	import ImageCollection from '$components/ImageCollection.svelte';
 	import type { CollectionQuery } from '$lib/sanity/queries';
 	import { setTitle } from '$lib/stores';
-	import ResponsiveImage from '../../../components/ResponsiveImage/ResponsiveImage.svelte';
 
 	export let data: CollectionQuery;
 
 	setTitle(data.title);
 </script>
 
-<div class="images">
-	{#each data.images as image}
-		<ResponsiveImage {image} sizes="50vh" alt="Nick LaVecchia" />
-	{/each}
-</div>
-
-<style lang="scss">
-	.images {
-		:global(img) {
-			/* height: 80vh;
-			width: auto; */
-		}
-	}
-</style>
+<ImageCollection {data} />
