d9c52b6741c4abfff34aab340b148aa10864f5e4
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-08-06 20:42:00 -0700

    feat: create responsive preloader

---
+++ web/src/components/ResponsiveImage/ResponsivePreload.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { urlFor } from '$lib/sanity/client';
+	import { DEFAULT_QUALITY } from './utils/constants';
+	import getSrcset from './utils/getSrcset';
+	import type { SanityImage } from './utils/types';
+
+	export let sizes: string;
+	export let image: SanityImage;
+
+	export let enforcedAspect: number | null = null;
+	export let quality = DEFAULT_QUALITY;
+
+	$: srcset = getSrcset(image, { quality, enforcedAspect });
+</script>
+
+<link
+	rel="preload"
+	as="image"
+	href={urlFor(image).url()}
+	imagesrcset={srcset}
+	imagesizes={sizes}
+/>
--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Nav } from './Nav.svelte';
 export { default as ResponsiveImage } from './ResponsiveImage/ResponsiveImage.svelte';
+export { default as ResponsivePreload } from './ResponsiveImage/ResponsivePreload.svelte';
 export { default as ImageGrid } from './ImageGrid.svelte';
 export { default as PageTransition } from './PageTransition.svelte';
 export { default as ImageCollection } from './ImageCollection.svelte';
