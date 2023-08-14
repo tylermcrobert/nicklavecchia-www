4e4c242f3617b5398615ba6f56b86da2da97ab03
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-08-13 22:51:16 -0700

    feat: make columns 2col mobile

--- web/src/components/ImageGrid.svelte
+++ web/src/components/ImageGrid.svelte
@@ -3,13 +3,19 @@
 </div>
 
 <style lang="scss">
+	@import '../styles/mixins';
+
 	.imageGrid {
 		gap: var(--space-standard);
 		padding: var(--space-standard);
 		padding-top: var(--space-large);
 
 		display: grid;
-		grid-template-columns: repeat(4, 1fr);
+		grid-template-columns: repeat(2, 1fr);
 		gap: var(--space-large) var(--space-standard);
+
+		@include min-width('tablet') {
+			grid-template-columns: repeat(4, 1fr);
+		}
 	}
 </style>
